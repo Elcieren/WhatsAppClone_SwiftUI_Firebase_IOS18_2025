6b16e191891504fd2e06abd8473f9994b0265adb
Elcieren <elcierenn@gmail.com>
2025-02-15 02:12:20 +0300

    Initial Commit

---
+++ WhatsAppClone_SwiftUI_Firebase_IOS18_2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsAppClone_SwiftUI_Firebase_IOS18_2025
+//
+//  Created by Eren Elçi on 15.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsAppClone_SwiftUI_Firebase_IOS18_2025/WhatsAppClone_SwiftUI_Firebase_IOS18_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppClone_SwiftUI_Firebase_IOS18_2025App.swift
+//  WhatsAppClone_SwiftUI_Firebase_IOS18_2025
+//
+//  Created by Eren Elçi on 15.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppClone_SwiftUI_Firebase_IOS18_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
